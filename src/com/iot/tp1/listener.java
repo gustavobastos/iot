017038c12e1d63b879131f832b6523ce47357ad9
gustavobastos <gustavosoad@gmail.com>
2015-03-29 13:11:21 -0300

    Create listener.java

---
+++ src/com/iot/tp1/listener.java
@@ -0,0 +1,109 @@
+package tp1;
+
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import com.alien.enterpriseRFID.tags.*;
+import com.alien.enterpriseRFID.notify.*;
+
+
+
+
+public class listener implements MessageListener {
+	int sucesso,falha,mensagensRec;
+	ArrayList<String> lista;
+	public listener() throws Exception {
+
+		sucesso = 0;
+		falha = 0;
+		mensagensRec = 0;
+		lista = new ArrayList<String>();
+		MessageListenerService service = new MessageListenerService(5000);
+		service.setMessageListener(this);
+		service.startService();
+		System.out.println("Message Listener has Started");
+
+		long runTime = 12000; // milliseconds
+		long startTime = System.currentTimeMillis();
+		do {
+			Thread.sleep(12000);
+		} while(service.isRunning() && (System.currentTimeMillis()-startTime) < runTime);
+
+
+
+
+
+		JTextArea textArea = new JTextArea(250,90);
+		JScrollPane scroll = new JScrollPane(textArea);
+		for(int i = 0; i < lista.size(); i++){
+			textArea.append(lista.get(i));
+
+		}
+		
+		textArea.setVisible(true);
+		textArea.setBorder(BorderFactory.createLineBorder(Color.gray));
+		textArea.setLineWrap(true);
+		textArea.setFocusable(true);
+
+		JScrollPane scroller = new JScrollPane(textArea);
+		JScrollBar bar = new JScrollBar();
+		
+		
+		JFrame janela = new JFrame("Modo Autonomo");
+		janela.add(scroller);
+		janela.pack();
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+
+
+
+		textArea.setCaretPosition(textArea.getText().length());	
+
+		System.out.println("Tentativas com Sucesso: " + 
+				sucesso);
+		System.out.println("Tentativas sem Sucesso: " + 
+				falha);
+
+
+	}
+
+	public void messageReceived(Message message){
+		System.out.println("\nMessage Received:");
+		mensagensRec++;
+		lista.add("\nMessagem recebida: \n");
+		if (message.getTagCount() == 0) {
+			System.out.println("(No Tags)\n");
+			falha++;
+			lista.add("No Tags\n");
+		} else {
+			for (int i = 0; i < message.getTagCount(); i++) {
+				Tag tag = message.getTag(i);
+				System.out.println(tag.toLongString());
+				sucesso++;
+				lista.add(tag.toLongString()+"\n");
+
+
+			}
+
+		}
+		lista.add("\n ---------------------------------- \n");
+	}
+
+	public static final void main(String args[]){
+		try {
+			new listener();
+		} catch (Exception e) {
+			System.out.println("Error:" + e.toString());
+		}
+	}
+
+} 
