96867635c7583b1f7ae28c4ef11e8cc3270da7c6
gustavobastos <gustavosoad@gmail.com>
2015-04-02 15:06:50 -0300

    Update reader.java

--- src/com/iot/tp1/reader.java
+++ src/com/iot/tp1/reader.java
@@ -1,7 +1,15 @@
 package tp1;
 
 
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import com.alien.enterpriseRFID.reader.*;
 import com.alien.enterpriseRFID.tags.*;
@@ -9,7 +17,10 @@ import com.alien.enterpriseRFID.*;
 
 public class reader {
 
+
+
 public  reader(String IpR) throws AlienReaderException{
+
 	
 	AlienClass1Reader reader = new AlienClass1Reader();
 	
@@ -19,32 +30,48 @@ public  reader(String IpR) throws AlienReaderException{
 	reader.setPassword("password");
 	
 	
+	JTextArea textArea = new JTextArea(20,40);
+	
+	
+
+	textArea.setVisible(true);
+	textArea.setBorder(BorderFactory.createLineBorder(Color.gray));
+	textArea.setLineWrap(true);
+	textArea.setFocusable(true);
+
+	JScrollPane scroller = new JScrollPane(textArea);
+	JFrame janela = new JFrame("Modo Manual");
+	janela.add(scroller);
+	janela.pack();
+	janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	janela.setVisible(true);
+
+
+
+	textArea.setCaretPosition(textArea.getText().length());	
+  
+	
 	reader.open();
 	
 	Tag tagList[] = reader.getTagList();
 	  if (tagList == null) {
-	    System.out.println("No Tags Found");
-	    JOptionPane.showMessageDialog(null, "Nenhuma tag encontrada!");
+	    textArea.append("Nenhuma tag encontrada!\n\n");
 	  } else {
-	    System.out.println("Tag(s) found:");
 	    for (int i=0; i<tagList.length; i++) {
 	      Tag tag = tagList[i];
-	      System.out.println("ID:" + tag.getTagID() +
-	                         ", Discovered:" + tag.getDiscoverTime() +
-	                         ", Last Seen:" + tag.getRenewTime() +
-	                         ", Antenna:" + tag.getAntenna() +
-	                         ", Reads:" + tag.getRenewCount()
-	                         );
-	      JOptionPane.showMessageDialog(null, "ID: " + tag.getTagID()+"\n" +
+	      textArea.append("ID: " + tag.getTagID()+"\n" +
                   "Discovered:" + tag.getDiscoverTime() +"\n" +
                   "Last Seen:" + tag.getRenewTime() +"\n" +
                   "Antenna:" + tag.getAntenna() +"\n" +
-                  "Reads:" + tag.getRenewCount()
+                  "Reads:" + tag.getRenewCount()+"\n-------------------------------------------\n\n"
                   );
 	    }
 	  }
 	  
+	  
 	  reader.close();
+	  
+	  
 	
 }
 
