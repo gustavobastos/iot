0fdeae9c8456ad109fbe1b3e0639e70d20896193
gustavobastos <gustavosoad@gmail.com>
2015-04-06 20:26:04 -0300

    Update Frame1.java

--- src/com/iot/tp1/Frame1.java
+++ src/com/iot/tp1/Frame1.java
@@ -1,18 +1,30 @@
 package tp1;
 
-import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+
+
+
+
 import javax.swing.JFrame;
+import javax.swing.JButton;
+
+import com.alien.enterpriseRFID.reader.AlienReaderException;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 import javax.swing.JLabel;
+
+import java.awt.Font;
+
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
-import com.alien.enterpriseRFID.reader.AlienReaderException;
+import java.awt.SystemColor;
+
+import javax.swing.UIManager;
 
 public class Frame1 {
 
@@ -56,7 +68,7 @@ public class Frame1 {
 		frmLeitorDeRfid.getContentPane().setLayout(null);
 
 		JButton btnManual = new JButton("Ir");
-
+		
 		btnManual.setBackground(Color.LIGHT_GRAY);
 		btnManual.setBounds(198, 115, 63, 23);
 		frmLeitorDeRfid.getContentPane().add(btnManual);
@@ -64,7 +76,7 @@ public class Frame1 {
 		JButton btnAutonomo = new JButton("Ir");
 
 		btnAutonomo.setBackground(Color.LIGHT_GRAY);
-		btnAutonomo.setBounds(85, 315, 108, 23);
+		btnAutonomo.setBounds(85, 296, 108, 23);
 		frmLeitorDeRfid.getContentPane().add(btnAutonomo);
 
 		JLabel lblSelecioneOModo = new JLabel("Modo de leitura MANUAL:");
@@ -73,7 +85,7 @@ public class Frame1 {
 		frmLeitorDeRfid.getContentPane().add(lblSelecioneOModo);
 
 		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 155, 273, 2);
+		separator.setBounds(10, 175, 273, 2);
 		frmLeitorDeRfid.getContentPane().add(separator);
 
 		textField = new JTextField();
@@ -96,57 +108,60 @@ public class Frame1 {
 		lblTempo.setBounds(30, 256, 46, 14);
 		frmLeitorDeRfid.getContentPane().add(lblTempo);
 
-		JLabel lblRepeticao = new JLabel("Repetições");
-		lblRepeticao.setFont(new Font("Times New Roman", Font.PLAIN, 11));
-		lblRepeticao.setBounds(30, 284, 50, 14);
-		frmLeitorDeRfid.getContentPane().add(lblRepeticao);
-
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(85, 282, 54, 20);
-		textField_3.setText("5");
-		frmLeitorDeRfid.getContentPane().add(textField_3);
-
 		JLabel lblEx = new JLabel("ex: 127.0.0.1:23");
 		lblEx.setBounds(160, 244, 88, 14);
 		frmLeitorDeRfid.getContentPane().add(lblEx);
-
+		
 		JLabel lblDigiteOEndereo = new JLabel("Digite o endere\u00E7o do leitor:");
 		lblDigiteOEndereo.setFont(new Font("DialogInput", Font.BOLD, 12));
 		lblDigiteOEndereo.setBounds(10, 26, 241, 14);
 		frmLeitorDeRfid.getContentPane().add(lblDigiteOEndereo);
-
+		
 		textField_2 = new JTextField();
+		textField_2.setText("150.164.9.35:23");
 		textField_2.setColumns(10);
 		textField_2.setBounds(10, 51, 163, 20);
 		frmLeitorDeRfid.getContentPane().add(textField_2);
-
+		
 		JLabel label = new JLabel("ex: 127.0.0.1:23");
 		label.setBounds(85, 71, 88, 14);
 		frmLeitorDeRfid.getContentPane().add(label);
-
+		
 		JLabel lblModoDeLeitura = new JLabel("Modo de leitura AUTON\u00D4MICO:");
 		lblModoDeLeitura.setFont(new Font("DialogInput", Font.BOLD, 12));
 		lblModoDeLeitura.setBounds(10, 188, 241, 14);
 		frmLeitorDeRfid.getContentPane().add(lblModoDeLeitura);
-
+		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(10, 96, 273, 2);
 		frmLeitorDeRfid.getContentPane().add(separator_1);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(85, 153, 38, 20);
+		frmLeitorDeRfid.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblRepeties = new JLabel("Repeti\u00E7\u00F5es");
+		lblRepeties.setFont(new Font("DialogInput", Font.BOLD, 12));
+		lblRepeties.setBounds(10, 155, 241, 14);
+		frmLeitorDeRfid.getContentPane().add(lblRepeties);
 
 		btnManual.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					new reader(textField_2.getText());
+					new reader(textField_2.getText(), textField_3.getText());
 
 				} catch (AlienReaderException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		});
-
-
+		
+		
 		btnAutonomo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
